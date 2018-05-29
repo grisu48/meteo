d39aabeb79a431f89dfeef48d0206a4d1be3ff2a
phoenix <felix@feldspaten.org>
2018-05-29 11:00:04 +0200

    http update

--- server.go/meteod.go
+++ server.go/meteod.go
@@ -4,15 +4,17 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"os/signal"
 	"time"
-	"syscall"
+	//"os/signal"
+	//"syscall"
 	"sync"
 	"strconv"
 	"github.com/eclipse/paho.mqtt.golang"
 	"github.com/buger/jsonparser"
 	"database/sql"
 	_ "github.com/mattn/go-sqlite3"
+	"net/http"
+	//"github.com/gorilla/mux"
 )
 
 
@@ -127,6 +129,7 @@ func pushStation(s station) {
 }
 
 func pushDB() {
+	//fmt.Println("Database thread started")
 	for {
 		time.Sleep(5 * time.Minute)			// Every 5 minutes
 		
@@ -160,6 +163,65 @@ func pushDB() {
 }
 
 
+//func www_handler_index(w http.ResponseWriter, r *http.Request) {
+func www_handler_index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<html><head><meta http-equiv=\"refresh\" content=\"30\"><title>meteo</title></head>\n<body>")
+	fmt.Fprintf(w, "<h1>meteo Web Portal</h1>\n")
+	fmt.Fprintf(w, "<p><a href=\"stations\">[Stations]</a> <a href=\"lightnings\">[Lightnings]</a></p>\n")
+	
+	// List all stations
+	mutex.Lock()
+	if len(stations) == 0 {
+		fmt.Fprintf(w, "<p>Currently no stations active</p>\n")
+	} else {
+		fmt.Fprintf(w, "<table border=\"1\">\n")
+		fmt.Fprintf(w, "<tr><td><b>Station</b></td><td><b>Temperature [deg C]</b></td><td><b>Pressure [hPa]</b></td><td><b>Humditiy [rel]</b></td></tr>\n")
+		for id, s := range stations { 
+			fmt.Fprintf(w, "<tr><td><a href=\"station?id=%d\">%s</a></td><td>%f</td><td>%f</td><td>%f</td></tr>\n", id, s.name, s.t, s.p, s.hum)
+		}
+		fmt.Fprintf(w, "</table>")
+		fmt.Fprintf(w, "<p>Active stations: <b>%d</b></p>\n", len(stations))
+	}
+	mutex.Unlock()
+	fmt.Fprintf(w, "<hr/><p><a href=\"https://github.com/grisu48/meteo\" target=\"_BLANK\">meteo</a>, 2018 phoenix</p>")
+	fmt.Fprintf(w, "</body></html>")
+}
+
+
+func www_handler_station(w http.ResponseWriter, r *http.Request) {
+	
+	s_id := r.URL.Query().Get("id")
+	if len(s_id) == 0 {
+		w.WriteHeader(http.StatusInternalServerError)
+	    w.Write([]byte("500 - No station id defined"))
+		return;
+	}
+	id,err := strconv.Atoi(s_id)
+	
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	    w.Write([]byte("500 - Illegal station identifier"))
+		return
+	}
+	
+	mutex.Lock()
+	station, exists := stations[id]
+	mutex.Unlock()
+	
+	if !exists {
+		w.WriteHeader(http.StatusInternalServerError)
+	    w.Write([]byte("500 - No such station active"))
+		return
+	}
+	
+	fmt.Fprintf(w, "<html><head><title>meteo</title></head>\n<body>")
+	fmt.Fprintf(w, "<h1>meteo Web Portal</h1>\n")
+	fmt.Fprintf(w, "<p><a href=\"stations\">[Stations]</a> <a href=\"lightnings\">[Lightnings]</a></p>\n")
+	fmt.Fprintf(w, "<h2>" + station.name + "</h2>\n")
+}
+
+
+
 func main() {
 	stations = make(map[int]station)
 	var err error
@@ -172,14 +234,15 @@ func main() {
 	
 	var host string = "127.0.0.1"
 	var port = 1883
+	//var wwwport = 8559
 	
 	if len(os.Args) > 1 {
 		host = os.Args[1]
 	}
 	
 	
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	//c := make(chan os.Signal, 1)
+	//signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	
 	//mqtt.DEBUG = log.New(os.Stderr, "", 0)			// Disable logging
 	mqtt.ERROR = log.New(os.Stderr, "", 0)
@@ -188,19 +251,35 @@ func main() {
 	opts.SetKeepAlive(30 * time.Second)
 	opts.SetPingTimeout(5 * time.Second)
 	opts.OnConnect = func(c mqtt.Client) {	
-		fmt.Println("Connected to " + remote)
+		fmt.Println("MQTT connected: " + remote)
 		if token := c.Subscribe("meteo/#", 0, onMessageReceived); token.Wait() && token.Error() != nil {
 			fmt.Println(token.Error())
 			os.Exit(1)
 		}
 	}
 
+	fmt.Println("Setting up mosquitto ...")
 	client := mqtt.NewClient(opts)
 	if token := client.Connect(); token.Wait() && token.Error() != nil {
 		panic(token.Error())
 	}
 	
 	go pushDB()		// Fork push DB thread
-	<-c
+	
+	// Setup webserver
+	//fmt.Println("Firing up webserver ... ")
+	/*
+	rtr := mux.NewRouter()
+	rtr.HandleFunc("/", www_handler_index).Methods("GET")
+	rtr.HandleFunc("/index.htm", www_handler_index).Methods("GET")
+	rtr.HandleFunc("/index.html", www_handler_index).Methods("GET")
+	*/
+	http.HandleFunc("/", www_handler_index)
+	http.HandleFunc("/stations", www_handler_index)
+	http.HandleFunc("/station", www_handler_station)
+	fmt.Println("Webserver started: http://127.0.0.1:8558")
+	http.ListenAndServe(":8558", nil)
+	
+	//<-c
 	fmt.Println("Bye")
 }
